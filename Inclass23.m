491771945b90c929e3655c346dffb9475a489b46
darlanminussi <darlan.minussi@uth.tmc.edu>
2017-11-28 12:55:07 -0600

    inclass23-darlanminussi

--- Inclass23.m
+++ Inclass23.m
@@ -7,11 +7,76 @@
 % 1. Start with initial conditions x(1) = 1, x(2) = x(3) = 0. 
 % Run the model for 200 time units and plot the result. verify that it
 % does indeed oscillate. 
+
+
+sol1 = ode23(@repressilator, [0 200], [1; 0; 0]);
+plot(sol1.x, sol1.y, '.-', 'LineWidth', 2, 'MarkerSize', 24);
+xlabel('Time'); ylabel('x,y'); set(gca, 'FontSize', 24);
+
 % 2. What happens if you start with initial conditions x(1) = x(2) = x(3) =
 % 0.5? Why?
+
+sol1 = ode23(@repressilator, [0 200], [0.5; 0.5; 0.5]);
+plot(sol1.x, sol1.y, '.-', 'LineWidth', 2, 'MarkerSize', 24);
+xlabel('Time'); ylabel('x,y'); set(gca, 'FontSize', 24);
+
+% it becomes a flatline because all the coefficients for the function will
+% have the same multiplicative value
+
 % 3. The strength of the repression is governed by the constant k2 which is
 % the same for all the genes. What happens when you make k2 larger or
 % smaller? Find the approximate value of k2 for which the system no longer
 % oscillates. 
+
+colors = ['g.-', 'r.-', 'y.-','m.-','c.-','b.-','k.-'];
+
+for ii = 1:5:20
+    global k2
+    k2 = 0.5;
+    sol1 = ode23(@repressilatorq3, [0 200], [1; 0; 0]);
+    plot(sol1.x, sol1.y, colors(ii), 'LineWidth', 2, 'MarkerSize', 24); hold on;
+    legendInfo{ii} = ['k2: ' num2str(ii)];
+end
+
+% For values below 0.5 the function stops oscilating
+
+
 % 4. What happens when you make k2 larger or smaller for only one of the
 % genes? 
+
+colors = ['g.-', 'r.-', 'y.-','m.-','c.-','b.-','k.-'];
+
+for ii = 1:5:20
+    global k2
+    global k2_2
+    k2 = 5;
+    k2_2 = 0.5;
+    sol1 = ode23(@repressilatorq4, [0 200], [1; 0; 0]);
+    plot(sol1.x, sol1.y, colors(ii), 'LineWidth', 2, 'MarkerSize', 24); hold on;
+    legendInfo{ii} = ['k2: ' num2str(ii)];
+end
+
+for ii = 1:5:20
+    global k2
+    global k2_2
+    k2 = 5;
+    k2_2 = 50;
+    sol1 = ode23(@repressilatorq4, [0 200], [1; 0; 0]);
+    plot(sol1.x, sol1.y, colors(ii), 'LineWidth', 2, 'MarkerSize', 24); hold on;
+    legendInfo{ii} = ['k2: ' num2str(ii)];
+end
+
+
+for ii = 1:5:20
+    global k2
+    global k2_2
+    k2 = 5;
+    k2_2 = 1000;
+    sol1 = ode23(@repressilatorq4, [0 200], [1; 0; 0]);
+    plot(sol1.x, sol1.y, colors(ii), 'LineWidth', 2, 'MarkerSize', 24); hold on;
+    legendInfo{ii} = ['k2: ' num2str(ii)];
+end
+
+% for values < 0.5 the function stops oscilating. for values > 5 we start
+% to see a separation of the curves and as we increase even more the
+% function oscilates with a smaller frequency
---
+++ repressilatorq4.m
@@ -0,0 +1,10 @@
+function dx = repressilator(t,x)
+
+global k2;
+global k2_2;
+k = 0.1; n = 4;
+
+dx = zeros(3,1);
+dx(1) = k*(1-x(1))-k2*x(3)^n/(1+x(3)^n)*x(1);
+dx(2) = k*(1-x(2))-k2_2*x(1)^n/(1+x(1)^n)*x(2);
+dx(3) = k*(1-x(3))-k2*x(2)^n/(1+x(2)^n)*x(3);
